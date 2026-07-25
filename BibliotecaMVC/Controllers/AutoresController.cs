f35f13047a7bffd5cd74ac396a4a2b65d53625b9
José Siguenza <jgsiguenzas200@gmail.com>
2026-07-24 21:26:50 -0600

    Laboratorio Autores

---
+++ BibliotecaMVC/Controllers/AutoresController.cs
@@ -0,0 +1,68 @@
+﻿    using Microsoft.AspNetCore.Mvc;
+    using BibliotecaMVC.Models;
+
+    namespace BibliotecaMVC.Controllers
+    {
+        public class AutoresController : Controller
+        {
+            public IActionResult Index()
+            {
+                List<Autor> autores = new List<Autor>()
+                {
+                    new Autor
+                    {
+                        ID = 1,
+                        Nombre = "Gabriel",
+                        Apellido = "García Márquez",
+                        Nacionalidad = "Colombiana",
+                        FechaNacimiento = new DateTime(1927, 3, 6),
+                        Activo = false
+                    },
+
+                    new Autor
+                    {
+                        ID = 2,
+                        Nombre = "Isabel",
+                        Apellido = "Allende",
+                        Nacionalidad = "Chilena",
+                        FechaNacimiento = new DateTime(1942, 8, 2),
+                        Activo = true
+                    },
+
+                    new Autor
+                    {
+                        ID = 3,
+                        Nombre = "Salarrué",
+                        Apellido = "Salazar Arrué",
+                        Nacionalidad = "Salvadoreña",
+                        FechaNacimiento = new DateTime(1899, 10, 22),
+                        Activo = false
+                    },
+
+                    new Autor
+                    {
+                        ID = 4,
+                        Nombre = "Claudia",
+                        Apellido = "Lars",
+                        Nacionalidad = "Salvadoreña",
+                        FechaNacimiento = new DateTime(1899, 12, 20),
+                        Activo = false
+                    },
+
+                    new Autor
+                    {
+                        ID = 5,
+                        Nombre = "Roque",
+                        Apellido = "Dalton",
+                        Nacionalidad = "Salvadoreña",
+                        FechaNacimiento = new DateTime(1935, 5, 14),
+                        Activo = false
+                    }
+                };
+
+                ViewBag.Autores = autores;
+
+                return View();
+            }
+        }
+    }
---
+++ BibliotecaMVC/Controllers/LibrosController.cs
@@ -0,0 +1,40 @@
+﻿    using Microsoft.AspNetCore.Mvc;
+    using BibliotecaMVC.Models;
+
+    namespace BibliotecaMVC.Controllers
+    {
+        public class LibrosController : Controller
+        {
+            public IActionResult Index()
+            {
+                List<Libro> libros = new List<Libro>()
+                {
+                    new Libro
+                    {
+                        ID = 1,
+                        Titulo = "Clean Code",
+                        Autor = "Robert Martin",
+                        Categoria = "Programacion",
+                        Precio = 35.5,
+                        Disponible = true,
+                    },
+
+                    new Libro {
+
+                        ID = 2,
+                        Titulo = "100 anios de soledad",
+                        Autor = "Gabriel Garcia Marquez",
+                        Categoria = "Literatura",
+                        Precio = 18,
+                        Disponible = false,
+
+
+                    }
+                };
+            
+                ViewBag.Libros = libros;
+
+                return View();
+            }
+        }
+    }
